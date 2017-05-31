5929579d768b9af7a8a6462fdf138dc1ab987b45
guohai <guohai@Live.com>
2017-06-01 00:17:54 +0800

    add fetch

--- src/about.js
+++ src/about.js
@@ -1,11 +1,32 @@
 import React, { Component } from 'react';
 import './App.css';
+import 'whatwg-fetch'
 
 class About extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { current_user_url: '' };
+  }
+
+  componentDidMount() {
+    this.fetchData((json) => this.setState({ current_user_url: json.current_user_url }));
+  }
+
+  fetchData(callback) {
+    fetch('https://api.github.com')
+      .then(function (response) {
+        return response.json()
+      }).then(function (json) {
+        callback(json);
+      }).catch(function (ex) {
+        console.log('parsing failed', ex)
+      })
+  }
+
   render() {
     return (
       <div className="App">
-        About
+        {this.state.current_user_url}
       </div>
     );
   }
